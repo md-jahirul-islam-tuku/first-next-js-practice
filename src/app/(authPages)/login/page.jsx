8f0e8b8768422c245245f03d524b8263009c95c5
Tuku <tukuwebian@gmail.com>
2026-01-20 13:03:38 +0300

    next authentication start

--- src/app/(authPages)/login/page.jsx
+++ src/app/(authPages)/login/page.jsx
@@ -1,7 +1,11 @@
+"use client"
 import React from 'react'
+import { signIn } from "next-auth/react"
 
 export default function LoginPage() {
   return (
-    <div>LoginPage</div>
+    <div>
+      <button onClick={()=>signIn()} className='cursor-pointer'>Login</button>
+    </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -7,3 +7,7 @@ export default function Home() {
     </div>
   );
 }
+
+// MONGO_URI=mongodb+srv://userManagementDB:Qw2aKihyS5hqEPm0@cluster0.dvetdgy.mongodb.net/?appName=Cluster0
+// DB_NAME=usersDB
+// NEXT_PUBLIC_BASE_URL=https://first-next-js-topaz.vercel.app
